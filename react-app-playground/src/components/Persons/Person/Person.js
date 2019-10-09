71daf82c82dc066f35ad9d3cdbcc1e4e4d2e6a96
srohde <sven.rohde@fdmediagroep.nl>
2019-10-09 20:06:56 +0200

    section 90

--- react-app-playground/src/components/Persons/Person/Person.js
+++ react-app-playground/src/components/Persons/Person/Person.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
 import UserOutput from "../../Assignment_1/UserOutput";
 import UserInput from "../../Assignment_1/UserInput";
+import classes from "./Person.css";
 
-import classes from './Person.css';
+// const person = props => {
+//   console.log("[Person.js] render called ...")
+//   return (
+//     <div className={classes.Person}>
+//       <div onClick={props.click}>
+//         <UserOutput name={props.name} />
+//       </div>
+//       <UserInput name={props.name} change={props.change}/>
+//     </div>
+//   );
+// };
 
-const person = props => {
-  console.log("[Person.js] render called ...")
-  return (
-    <div className={classes.Person}>
-      <div onClick={props.click}>
-        <UserOutput name={props.name} />
+class Person extends Component {
+  render() {
+    console.log("[Person.js] rendering ...");
+    return (
+      <div className={classes.Person}>
+        <div onClick={this.props.click}>
+          <UserOutput name={this.props.name} />
+        </div>
+        <UserInput name={this.props.name} change={this.props.change} />
       </div>
-      <UserInput name={props.name} change={props.change}/>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default person;
+export default Person;
--- react-app-playground/src/components/Persons/Persons.js
+++ react-app-playground/src/components/Persons/Persons.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const persons = props =>
-  props.persons.map((person, index) => {
-    console.log("[Persons.js] render called ...")
-    return (
+// const persons = props =>
+//   props.persons.map((person, index) => {
+//     console.log("[Persons.js] render called ...")
+//     return (
+//         <Person
+//           click={() => props.click(index)}
+//           change={event => props.change(event, person.id)}
+//           key={person.id}
+//           name={person.name}
+//           age={person.age}
+//         />
+//     );
+//   });
+
+class Persons extends Component {
+
+    // is static
+    static getDerivedStateFromProps(props, state) {
+      console.log("[Persons.js] getDerivedStateFromProps called", props);
+      return state;
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+      console.log("[Persons.js] ShouldComponentUpdate called ...");
+      return true;
+    }
+
+    getSnapshotBeforeUpdate(prevtProps, prevState){
+      console.log("[Persons.js] getSnapshotBeforeUpdate called ...");
+      return {message: "Snapshot"};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+      console.log("[Persons.js] ComponentDidUpdate called ...");
+      console.log("[Persons.js] ComponentDidUpdate snapshot value: ", snapshot);
+    }
+
+  render() {
+    console.log("[Persons.js] rendering ...");
+    return this.props.persons.map((person, index) => {
+      return (
         <Person
-          click={() => props.click(index)}
-          change={event => props.change(event, person.id)}
+          click={() => this.props.click(index)}
+          change={event => this.props.change(event, person.id)}
           key={person.id}
           name={person.name}
           age={person.age}
         />
-    );
-  });
+      );
+    });
+  }
+}
 
-export default persons;
+export default Persons;
