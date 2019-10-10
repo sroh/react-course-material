0dec24211080766f59d939968856f2c1541ebc76
srohde <sven.rohde@fdmediagroep.nl>
2019-10-10 05:47:22 +0200

    section 158

--- burger-builder-react-app/src/components/Burger/Burger.js
+++ burger-builder-react-app/src/components/Burger/Burger.js
@@ -5,13 +5,21 @@ import classes from "./Burger.css";
 const burger = props => {
   // transform to Array from Object
   const transformedIngredientsArray = Object.keys(props.ingredients);
-  const transformedIngredients = transformedIngredientsArray.map(igKey => {
-
+  let transformedIngredients = transformedIngredientsArray
+    .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
-        return <BurgerIngredient key={igKey + i} type={igKey} />
-      })
-  })
-  
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+if(transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients to your custom made Burger !</p>
+}
+
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
